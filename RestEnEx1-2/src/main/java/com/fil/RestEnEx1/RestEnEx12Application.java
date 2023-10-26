4f46af5baaee42d5f7c2d29c593f5c661fa8f8d5
Pranjali <sharmapranjali2806@gmail.com>
2023-10-26 09:26:44 +0530

    Created spring starter project

---
+++ RestEnEx1-2/src/main/java/com/fil/RestEnEx1/RestEnEx12Application.java
@@ -0,0 +1,13 @@
+package com.fil.RestEnEx1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEnEx12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEnEx12Application.class, args);
+	}
+
+}
---
+++ RestEnEx1-2/src/test/java/com/fil/RestEnEx1/RestEnEx12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fil.RestEnEx1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestEnEx12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

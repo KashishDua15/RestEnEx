029bf66c0e06dc231019bc7d5b922fc5ac8b2737
Pranjali <sharmapranjali2806@gmail.com>
2023-10-26 16:39:33 +0530

    Refactored customer side entities to create relation between tables

--- RestEnEx1-2/src/main/java/com/fil/RestEnEx1/entities/Address.java
+++ RestEnEx1-2/src/main/java/com/fil/RestEnEx1/entities/Address.java
@@ -1,14 +1,32 @@
 package com.fil.RestEnEx1.entities;
 
-import jakarta.persistence.Embeddable;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
-@Embeddable
+@Entity
 public class Address {
+	@Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long addressId;
 	private String houseNumber;
 	private String street;
 	private String area;
 	private String city;
 	private long pincode;
+	@OneToOne
+    @JoinColumn(name="customerId", nullable=false)
+    private Customer customer;
+	
+	public long getAddressId() {
+		return addressId;
+	}
+	public void setAddressId(long addressId) {
+		this.addressId = addressId;
+	}
 	public String getHouseNumber() {
 		return houseNumber;
 	}
@@ -39,13 +57,20 @@ public class Address {
 	public void setPincode(long pincode) {
 		this.pincode = pincode;
 	}
-	public Address(String houseNumber, String street, String area, String city, long pincode) {
+	public Customer getCustomer() {
+		return customer;
+	}
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	public Address(String houseNumber, String street, String area, String city, long pincode, Customer customer) {
 		super();
 		this.houseNumber = houseNumber;
 		this.street = street;
 		this.area = area;
 		this.city = city;
 		this.pincode = pincode;
+		this.customer = customer;
 	}
 	public Address() {
 		super();
@@ -54,8 +79,7 @@ public class Address {
 	@Override
 	public String toString() {
 		return "Address [houseNumber=" + houseNumber + ", street=" + street + ", area=" + area + ", city=" + city
-				+ ", pincode=" + pincode + "]";
+				+ ", pincode=" + pincode + ", customer=" + customer + "]";
 	}
 	
-
 }

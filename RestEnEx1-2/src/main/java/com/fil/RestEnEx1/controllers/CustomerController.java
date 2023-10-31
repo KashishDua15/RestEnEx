43beac23476285d842e9a3f79a8024fac6933df0
Inshita12 <inshitabamba@gmail.com>
2023-10-31 11:16:02 +0530

    Getting values from form to database

--- RestEnEx1-2/src/main/java/com/fil/RestEnEx1/controllers/CustomerController.java
+++ RestEnEx1-2/src/main/java/com/fil/RestEnEx1/controllers/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -33,7 +34,7 @@ public class CustomerController {
 	}
 
 	@PostMapping("/customer/signup")
-	public String customerSignUp(@RequestBody Customer customer) {
+	public String customerSignUp(@ModelAttribute("customer") Customer customer) {
 		customerService.customerSignUp(customer);
 		return "SignUpCustomer";
 	}

b5f478b45e7ab165e61a52fcee00598ce9f9365b
Pranjali <Pranjali@192.168.1.11>
2023-10-31 22:27:19 +0530

    Added & Tested repeatLastOrder functionality for customer

--- RestEnEx1-2/src/main/java/com/fil/RestEnEx1/services/OwnerService.java
+++ RestEnEx1-2/src/main/java/com/fil/RestEnEx1/services/OwnerService.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 import com.fil.RestEnEx1.entities.Customer;
 import com.fil.RestEnEx1.entities.MenuItem;
 import com.fil.RestEnEx1.entities.Owner;
-import com.fil.RestEnEx1.entities.RestEnExOrders;
+import com.fil.RestEnEx1.entities.CustomerOrders;
 import com.fil.RestEnEx1.entities.Restaurant;
 
 public interface OwnerService {
@@ -19,7 +19,7 @@ public interface OwnerService {
 	public Restaurant updateTotalSeats(UUID restaurantId,int updateTotalSeats);
 	public void addMenuItem(UUID restaurantId,MenuItem menuItem);
 	public int getRating(UUID restaurantId);
-	public RestEnExOrders getOrder (UUID orderId);
-	public List<RestEnExOrders> getAllOrders();
+	public CustomerOrders getOrder (UUID orderId);
+	public List<CustomerOrders> getAllOrders();
 	
 }

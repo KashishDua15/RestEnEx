7012be196fb430a690f68d621a53d084833cef98
Suvamrx <suvamrx@gmail.com>
2023-11-01 12:06:32 +0530

    [RS-69]: Adding Encryption for the both Owner and Customer side

--- RestEnEx1-2/src/main/java/com/fil/RestEnEx1/services/OwnerServiceImpl.java
+++ RestEnEx1-2/src/main/java/com/fil/RestEnEx1/services/OwnerServiceImpl.java
@@ -84,7 +84,9 @@ public class OwnerServiceImpl implements OwnerService{
 	public Owner ownerSignIn(String email, String password) {
 		Owner owner = ownerDao.findByEmailId(email);
 		if(owner!=null) {
-			if(owner.getPassword().equals(password))
+			String encryptedpass = SHA256Util.getSHA256(password);
+			System.out.println("Encrpted"+encryptedpass);
+			if(owner.getPassword().equals(encryptedpass))
 				return owner;
 		}
 		return null;
@@ -96,6 +98,7 @@ public class OwnerServiceImpl implements OwnerService{
 		System.out.println(owner.getRestaurant());
 		Restaurant r = owner.getRestaurant();
 		restaurantDao.saveAndFlush(r);
+		owner.setPassword(SHA256Util.getSHA256(owner.getPassword()));
 		ownerDao.saveAndFlush(owner);
 	}
 
---
+++ RestEnEx1-2/src/main/java/com/fil/RestEnEx1/services/SHA256Util.java
@@ -0,0 +1,21 @@
+package com.fil.RestEnEx1.services;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256Util {
+
+    public static String getSHA256(String input) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] messageDigest = md.digest(input.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : messageDigest) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
